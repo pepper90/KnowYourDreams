42f1c4653bd1ea033b3ec6217c0c2a790e032327
pepper90 <panayotovj@gmail.com>
2021-08-02 22:05:21 +0300

    Updated Favourites fragment - Added Layout functionality Updated FavouritesAdapter.kt - Added Recyclerview functionality

--- app/src/main/java/com/jpdevzone/knowyourdreams/adapters/FavouritesAdapter.kt
+++ app/src/main/java/com/jpdevzone/knowyourdreams/adapters/FavouritesAdapter.kt
@@ -3,7 +3,6 @@ package com.jpdevzone.knowyourdreams.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
